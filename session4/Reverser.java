f68916f26648316c071a90c26150ba5446dac20a
Daniel Alabi <alabid@carleton.edu>
2014-04-13 22:13:54 -0500

    session 4 done

---
+++ session4/Reverser.java
@@ -0,0 +1,77 @@
+import java.util.*;
+
+public class Reverser {
+
+    public static String reverse(String toReverse) {
+	Stack<String> s = new Stack<String>();
+	for (int i = 0; i < toReverse.length();i++) {
+	    s.push("" + toReverse.charAt(i));
+	}
+	String empty = "";
+	while (!s.empty()) {
+	    empty += s.pop();
+	}
+	return empty;
+    }
+
+    public static boolean checkParens(String toCheck) {
+	Stack<String> s = new Stack<String>();
+	for (int i = 0; i < toCheck.length(); i++) {
+	    char c =  toCheck.charAt(i);
+	    if (c == '(') {
+		s.push("" + toCheck.charAt(i));
+	    } else {
+		if (s.empty()) {
+		    return false;
+		} else {
+		    s.pop();
+		}
+	    }
+	}
+	return s.empty();
+    }
+
+    public static void printStack(Stack<String> s) {
+	String string;
+	while (!s.empty()) {
+	    string = s.pop();
+	    System.out.println(string);
+	}
+    }
+
+    public static void main(String[] args) {
+	String simple = "abcdefghijklmnopq";
+	
+	System.out.println("Before reverse: " + simple);	
+
+	System.out.println("After reverse: " + reverse(simple));
+
+	String testParens1 = "()(((()())))";
+	String testParens2 = "(((((()()))";
+	String testParens3 = "())))";
+	System.out.println(testParens1);
+	System.out.println(checkParens(testParens1));// true
+
+	System.out.println(testParens2);
+	System.out.println(checkParens(testParens2));// false
+
+	System.out.println(testParens3);
+	System.out.println(checkParens(testParens3)); // false
+
+	Stack<String> s  = new Stack<String>();
+	s.push("john");
+	s.push("catie");
+	s.push("risako");
+	s.push("josh");
+	// printStack(s);
+	LinkedList<String> q = new LinkedList<String>();
+	while (!s.empty()) {
+	    q.addLast(s.pop());
+	}
+	while (!q.isEmpty()) {
+	    s.push(q.removeFirst());
+	}
+	printStack(s);
+	
+    }
+}
